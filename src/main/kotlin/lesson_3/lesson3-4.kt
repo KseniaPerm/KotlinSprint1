c69bf11f6b7b8e7290af5018a71251a37e7a6e9e
KPermyakova <k.e.permyakova@yandex.ru>
2025-03-04 15:00:00 +0500

    KS-3-4 chess moving

---
+++ src/main/kotlin/lesson_3/lesson3-4.kt
@@ -0,0 +1,10 @@
+package org.example.lesson_3
+
+fun main() {
+    var move = "E2-E4"
+    var strokeNumber = 1
+    println("$move,$strokeNumber")
+    move = "D2-D3"
+    strokeNumber = 2
+    println("$move,$strokeNumber")
+}
