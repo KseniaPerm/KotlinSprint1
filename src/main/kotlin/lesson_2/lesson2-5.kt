3106180ff16fb1e121575560365ca1e6f529e0f1
KPermyakova <k.e.permyakova@yandex.ru>
2025-03-04 17:23:46 +0500

    KS-2-5 change percent for deposit

--- src/main/kotlin/lesson_2/lesson2-5.kt
+++ src/main/kotlin/lesson_2/lesson2-5.kt
@@ -4,9 +4,9 @@ import kotlin.math.pow
 
 fun main() {
    val depositAmount = 70_000
-   val percent = 0.167
+   val percent = 16.7
    val period = 20
-   val compoundInterest = depositAmount * ((COMPOUND_INTEREST + percent).pow(period))
+   val compoundInterest = depositAmount * ((COMPOUND_INTEREST + percent/ 100).pow(period))
    println(String.format("%.3f",compoundInterest))
 }
 const val COMPOUND_INTEREST = 1
