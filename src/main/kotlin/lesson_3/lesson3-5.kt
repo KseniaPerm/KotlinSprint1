198a38e7f2d007da7552c20ce327cd16ebfe3aa3
KPermyakova <k.e.permyakova@yandex.ru>
2025-03-04 15:17:42 +0500

    KS-3-5 chess players move

---
+++ src/main/kotlin/lesson_3/lesson3-5.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_3
+
+fun main() {
+   var playersMove = "D2-D4;0"
+   var moveFrom = "D2"
+   var moveTo = "D4"
+   var strokeNumber = 0
+   println(moveFrom)
+   println(moveTo)
+   println(strokeNumber)
+}
