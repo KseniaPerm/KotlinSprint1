76be95709c1de61b49d50c5bc851daaeb4252132
KPermyakova <k.e.permyakova@yandex.ru>
2025-03-05 13:30:08 +0500

    KS-3-4 correcting errors by chess moves

--- src/main/kotlin/lesson_3/lesson3-4.kt
+++ src/main/kotlin/lesson_3/lesson3-4.kt
@@ -3,8 +3,10 @@ package org.example.lesson_3
 fun main() {
     var move = "E2-E4"
     var strokeNumber = 1
-    println("$move,$strokeNumber")
+
+    println("[$move;$strokeNumber]")
     move = "D2-D3"
-    strokeNumber = 2
-    println("$move,$strokeNumber")
+    strokeNumber++
+
+    println("[$move;$strokeNumber]")
 }
