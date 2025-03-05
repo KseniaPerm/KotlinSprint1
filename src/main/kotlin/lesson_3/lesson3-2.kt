cd16adc32a4e74facb0d0458bd7977c34775da0a
KPermyakova <k.e.permyakova@yandex.ru>
2025-03-05 13:15:34 +0500

    KS-3-2 change lastname correctly

--- src/main/kotlin/lesson_3/lesson3-2.kt
+++ src/main/kotlin/lesson_3/lesson3-2.kt
@@ -1,12 +1,12 @@
 package org.example.lesson_3
 
 fun main() {
-    val lastname = "Андреева "
-    val lastName2 = "Сидорова "
-    val name = "Татьяна "
+    var lastname = "Андреева"
+    val name = "Татьяна"
     val patronymic = "Сергеевна"
-    val age1 = 20
-    val age2 = 22
-    println("$lastname $name $patronymic,$age1")
-    println("$lastName2 $name $patronymic,$age2")
+    var age = 20
+    println("$lastname $name $patronymic, $age")
+    lastname = "Сидорова"
+    age = 22
+    println("$lastname $name $patronymic, $age")
 }
