61b03a5edbb4797ced23f0684275662bb921ebe0
KPermyakova <k.e.permyakova@yandex.ru>
2025-03-07 15:02:52 +0500

    KS-5-5-lottery-three-numbers

---
+++ src/main/kotlin/lesson_5/lesson5-5.kt
@@ -0,0 +1,43 @@
+package org.example.lesson_5
+
+import kotlin.random.Random
+
+fun main() {
+    val winningNumbers = mutableListOf<Int>()
+    winningNumbers.add(Random.nextInt(0, 42))
+    winningNumbers.add(Random.nextInt(0, 42))
+    winningNumbers.add(Random.nextInt(0, 42))
+    val userNumbers = mutableListOf<Int>()
+
+    println("Введите три числа от 0 до 42 .Введите первое число : ")
+
+    val userNumber1 = readln().toInt()
+    userNumbers.add(userNumber1)
+
+    println("Введите второе число : ")
+
+    val userNumber2 = readln().toInt()
+    userNumbers.add(userNumber2)
+
+    println("Введите третье число: ")
+
+    val userNumber3 = readln().toInt()
+    userNumbers.add(userNumber3)
+
+    println("Ваши введеные числа : $userNumbers")
+    val compare = userNumbers.intersect(winningNumbers.toSet()).size
+    if (compare == 1) {
+        println("Вы угадали одно число, для вас утешительный приз!")
+    } else if (compare == 2) {
+        println("Вы угадали два числа и получаете крупный приз!")
+    } else if (compare == 3) {
+        println("Вы угадали все числа и выиграли джекпот!")
+    } else {
+        println("Вы не угадали ни одного числа.")
+    }
+    println("Выигрышные числа : $winningNumbers")
+}
+
+
+
+
