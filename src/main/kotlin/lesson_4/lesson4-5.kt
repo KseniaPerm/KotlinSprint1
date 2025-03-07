27985b71950c6298a163193c8d159e2d1377bd83
KPermyakova <k.e.permyakova@yandex.ru>
2025-03-07 13:19:12 +0500

    KS-4-5 using constant and change string input

--- src/main/kotlin/lesson_4/lesson4-5.kt
+++ src/main/kotlin/lesson_4/lesson4-5.kt
@@ -1,16 +1,17 @@
 package org.example.lesson_4
 
 fun main() {
-    val serviceAbility = readlnOrNull().toBoolean()
-    val numberOfCrew = readlnOrNull()!!.toInt()
-    val numberOfBox = readlnOrNull()!!.toInt()
-    val weatherConditions = readlnOrNull()!!.toBoolean()
+    println("Введите количество человек в экипаже: ")
+    val numberOfCrew = readln().toInt()
+    println("Введите количество ящиков провизии: ")
+    val numberOfBox = readln().toInt()
+    println("Введите информацию по благоприятности метеоусловий (true/false): ")
+    val weatherConditions = readln()
 
-    val goSailing: Boolean = (!serviceAbility) && (numberOfCrew in NUMBER_OF_CREW_LOW..NUMBER_OF_CREW_HIGH) &&
-            (numberOfBox > NUMBER_OF_BOX) && (weatherConditions) || (serviceAbility) &&
-            (numberOfCrew == RECOMMENDED_CREW) && (weatherConditions) &&
+    val goSailing: Boolean = (!SERVICE_ABILITY) && (numberOfCrew in NUMBER_OF_CREW_LOW..NUMBER_OF_CREW_HIGH) &&
+            (numberOfBox > NUMBER_OF_BOX) && (weatherConditions == WEATHER_CONDITIONS.toString()) || (SERVICE_ABILITY) &&
+            (numberOfCrew == RECOMMENDED_CREW) && (weatherConditions == WEATHER_CONDITIONS.toString()) &&
             (numberOfBox >= NUMBER_OF_BOX)
-
     println(goSailing)
 }
 
@@ -18,8 +19,5 @@ const val NUMBER_OF_CREW_LOW = 55
 const val NUMBER_OF_CREW_HIGH = 70
 const val NUMBER_OF_BOX = 50
 const val RECOMMENDED_CREW = 70
-
-
-
-
-
+const val SERVICE_ABILITY = false
+const val WEATHER_CONDITIONS = true
