5c8d826b219f444a16d54ae164ba9b8015dda45c
KPermyakova <k.e.permyakova@yandex.ru>
2025-03-10 17:01:47 +0500

    KS-4-5 changing constants in use

--- src/main/kotlin/lesson_4/lesson4-5.kt
+++ src/main/kotlin/lesson_4/lesson4-5.kt
@@ -6,12 +6,15 @@ fun main() {
     println("Введите количество ящиков провизии: ")
     val numberOfBox = readln().toInt()
     println("Введите информацию по благоприятности метеоусловий (true/false): ")
-    val weatherConditions = readln()
+    val weatherConditions = readln().toBoolean()
+    println("Введите исправность корабля (true/false)  ")
+    val serviceAbility = readln().toBoolean()
 
-    val goSailing: Boolean = (!SERVICE_ABILITY) && (numberOfCrew in NUMBER_OF_CREW_LOW..NUMBER_OF_CREW_HIGH) &&
-            (numberOfBox > NUMBER_OF_BOX) && (weatherConditions == WEATHER_CONDITIONS.toString()) || (SERVICE_ABILITY) &&
-            (numberOfCrew == RECOMMENDED_CREW) && (weatherConditions == WEATHER_CONDITIONS.toString()) &&
-            (numberOfBox >= NUMBER_OF_BOX)
+    val goSailing: Boolean =
+        (serviceAbility != SERVICE_ABILITY) && (numberOfCrew in NUMBER_OF_CREW_LOW..NUMBER_OF_CREW_HIGH) &&
+                (numberOfBox > NUMBER_OF_BOX) && (weatherConditions == WEATHER_CONDITIONS) || (serviceAbility == SERVICE_ABILITY) &&
+                (numberOfCrew == RECOMMENDED_CREW) && (weatherConditions == WEATHER_CONDITIONS) &&
+                (numberOfBox >= NUMBER_OF_BOX)
     println(goSailing)
 }
 
