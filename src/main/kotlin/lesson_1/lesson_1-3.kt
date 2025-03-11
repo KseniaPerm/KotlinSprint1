4659604c2809662924528e9f83899d7cf38163bd
KPermyakova <k.e.permyakova@yandex.ru>
2025-03-11 12:06:45 +0500

    KS-1-3 add right string with 0

--- src/main/kotlin/lesson_1/lesson_1-3.kt
+++ src/main/kotlin/lesson_1/lesson_1-3.kt
@@ -2,14 +2,14 @@ package org.example.lesson_1
 
 fun main() {
     val year = 1961
-    val str = "0"
-    var hour = 9
-    var minute = 7
-    println(year)
-    println("$str$hour")
-    println("$str$minute")
+    var hour = "09"
+    var minute = "07"
 
-    hour = 10
-    minute = 55
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = "10"
+    minute = "55"
     println("$hour:$minute")
 }
