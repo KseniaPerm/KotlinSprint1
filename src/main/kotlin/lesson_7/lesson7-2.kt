c508002883cff0eaa7db3a458846417b885ee0c2
KPermyakova <k.e.permyakova@yandex.ru>
2025-03-17 16:21:12 +0500

    KS-7-2-authorization-by-sms

---
+++ src/main/kotlin/lesson_7/lesson7-2.kt
@@ -0,0 +1,19 @@
+package org.example.lesson_7
+
+import kotlin.random.Random
+
+fun main() {
+    while (true) {
+        val randomNumber = Random.nextInt(1000, 9999)
+        println("Ваш код авторизации $randomNumber")
+
+        println("Введите код авторизации: ")
+        val authorizationCode = readln().toInt()
+        if (randomNumber == authorizationCode) {
+            println("Приветствую!")
+            return
+        } else {
+            println("Код введен неверно! Введите верный код ")
+        }
+    }
+}
