6ed2b22c576fc00fd0a60684867289b62039b435
KPermyakova <k.e.permyakova@yandex.ru>
2025-03-17 12:16:35 +0500

    KS-6-2 countdown-timer without while

--- src/main/kotlin/lesson_6/lesson6-2.kt
+++ src/main/kotlin/lesson_6/lesson6-2.kt
@@ -2,12 +2,8 @@ package org.example.lesson_6
 
 fun main() {
     println("Введите количество секунд, которое нужно засечь : ")
-    var seconds = readln().toInt()
-    val seconds1 = seconds
-    while (seconds > 0) {
-        println("Прошло ${seconds--} секунд")
-        Thread.sleep(1000)
-    }
-    println()
-    println("Прошло $seconds1 секунд")
+    val seconds = readln().toInt()
+    Thread.sleep(1000)
+    println("Прошло $seconds секунд")
 }
+
