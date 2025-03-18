c3c8453f127a0297b1514c33eb4ad9fc6119f127
KPermyakova <k.e.permyakova@yandex.ru>
2025-03-18 12:27:02 +0500

    KS-7-1 generate password with empty string

--- src/main/kotlin/lesson_7/lesson7-1.kt
+++ src/main/kotlin/lesson_7/lesson7-1.kt
@@ -1,13 +1,20 @@
 package org.example.lesson_7
 
+import kotlin.random.Random
 
 fun main() {
-    val range1 = ('1'..'9') + ('a'..'z')
-    val password = StringBuilder(5)
-    for (i in 0..5) {
-        val randomChars = range1.random()
-        password.append(randomChars)
+    val range1 = ('1'..'9').toList()
+    val range2 = ('a'..'z').toList()
+    var password = " "
+
+    for (i in 1..6) {
+        if (i % 2 == 0) {
+            val figure = range1[Random.nextInt(range1.size)]
+            password += figure
+        } else {
+            val cymbol = range2[Random.nextInt(range2.size)]
+            password += cymbol
+        }
     }
     println(password)
 }
-
