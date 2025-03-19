c12be237f45bf438b0c6442d8bb201b2b01ab9f1
KPermyakova <k.e.permyakova@yandex.ru>
2025-03-19 12:27:36 +0500

    KS-8-5-input-ingredients-from-user fixed

--- src/main/kotlin/lesson_8/lesson8-5.kt
+++ src/main/kotlin/lesson_8/lesson8-5.kt
@@ -1,30 +1,7 @@
 package org.example.lesson_8
 
-
 fun main() {
-
-
-    println("Введите количество ингредиентов: ")
-    val numberOfIngredients = readln().toInt()
-
-    var arrayOfIngredients = arrayOf<String>()
-    for (i in 0..<numberOfIngredients) {
-        println("Введите название ингредиента: ")
-        arrayOfIngredients = arrayOf(readln())
-    }
-    println(arrayOfIngredients.joinToString(","))
+    println("Введите количество и названия ингредиентов : ")
+    val arrayOfIngredients = Array(readln().toInt()) { readln() }
+    println("Ингредиенты : ${arrayOfIngredients.joinToString()}")
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
