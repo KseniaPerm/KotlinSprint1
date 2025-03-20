b12d47b9b94dc2ad52887b6320726d4f99c00c02
KPermyakova <k.e.permyakova@yandex.ru>
2025-03-20 16:47:21 +0500

    KS9-4-requesting-ingredients-from-user forEach

--- src/main/kotlin/lesson_9/lesson9-4.kt
+++ src/main/kotlin/lesson_9/lesson9-4.kt
@@ -4,7 +4,7 @@ fun main() {
     println("Введите пять названий блюд, через запятую с пробелом : ")
     val userIngredient = readln()
     val listOfIngredients = userIngredient.split(' ')
-    listOfIngredients.sorted().map {
+    listOfIngredients.sorted().forEach {
         println(it)
     }
 }
