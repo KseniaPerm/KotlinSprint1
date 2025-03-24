cca4ef71a159ce66762ac66c8db8c5ec7c4f7b60
KPermyakova <k.e.permyakova@yandex.ru>
2025-03-24 13:44:24 +0500

    KS-10-4-Advanced-dice-rolling-game

---
+++ src/main/kotlin/lesson_10/lesson10-4.kt
@@ -0,0 +1,36 @@
+package org.example.lesson_10
+
+import kotlin.random.Random
+
+fun main() {
+    var userWin = 0
+    do {
+        val rollUser = rollOfDices()
+        printThrowRolls("Пользователь", rollUser)
+
+        val rollComputer = rollOfDices()
+        printThrowRolls("Компьютер", rollComputer)
+
+        if (rollComputer > rollUser) {
+            println()
+            println("Победила машина")
+        } else if (rollComputer < rollUser) {
+            println()
+            println("Победило человечество")
+            userWin++
+        } else {
+            println("Победила дружба ")
+        }
+        println("Хотите бросить кубики еще раз? ")
+        val answer = readln()
+    } while (answer.equals("да", ignoreCase = true))
+    println("Вы выиграли партий - $userWin ")
+}
+
+fun rollOfDices(): Int {
+    return Random.nextInt(1, 6)
+}
+
+fun printThrowRolls(player: String, throwRoll: Int) {
+    println("Бросок $player.\nНа кубиках выпало значение $throwRoll")
+}
