ee69e95c985abda40c864b9de5df5c3fd5206b92
KPermyakova <k.e.permyakova@yandex.ru>
2025-03-31 16:30:47 +0500

    KS-11-3-creating-room-for-communication with two classes

--- src/main/kotlin/lesson_11/lesson11-3.kt
+++ src/main/kotlin/lesson_11/lesson11-3.kt
@@ -1,11 +1,29 @@
 package org.example.lesson_11
 
+class User(
+    val userName: String,
+    var userStatus: String,
+    val profilePicture: String,
+) {
+    fun printUserInformation() {
+        println("Информация о пользователе: ")
+        println(userName)
+        println(userStatus)
+        println(profilePicture)
+    }
+
+    fun updateStatus(user: String, newStatus: String) {
+        if (user == userName) {
+            userStatus = newStatus
+            println("Статус обновлен на $newStatus")
+        }
+    }
+}
+
 class Room(
     val cover: String,
     var title: String,
-    var user: String,
-    var listOfUsers: MutableList<String>,
-    var userStatus: String,
+    var listOfUsers: MutableList<String> = mutableListOf(),
 ) {
     fun printRoomInformation() {
         println("Информация о комнате: ")
@@ -14,38 +32,39 @@ class Room(
         println(listOfUsers.joinToString())
     }
 
-    fun printUserInformation() {
-        println("Информация о пользователе: ")
-        println(user)
-        println(userStatus)
+    fun addUser(user: String) {
+        if (user in listOfUsers) {
+            println("Участник уже зарагестрирован")
+        } else {
+            listOfUsers.add(user)
+            println("Участник $user добавлен в список")
+        }
     }
 
-    fun addUser() {
-        println("Введите имя участника: ")
-        val userName = readln()
-        listOfUsers.add(userName)
-        println("Участник $userName добавлен в список ")
-    }
-
-    fun updateStatus() {
-        println("Введите новый статус: разговаривает, микрофон выключен, пользователь заглушен")
-        val newUserStatus = readln()
-        userStatus = newUserStatus
-    }
 }
 
 fun main() {
-
-    user3.printRoomInformation()
-    user3.printUserInformation()
-    user3.addUser()
-    user3.updateStatus()
-    user3.printUserInformation()
+    room.printRoomInformation()
+    user.printUserInformation()
+    println("Введите имя участника:")
+    val userName = readln()
+    println("Введите новый статус: разговаривает/ микрофон выключен / пользователь заглушен")
+    val newStatus = readln()
+    user.updateStatus(userName, newStatus)
+    user.printUserInformation()
+    println("Добавьте нового пользователя. Введите имя: ")
+    val user = readln()
+    room.addUser(user)
+    room.printRoomInformation()
 }
-val user3 = Room(
+
+val room = Room(
     cover = "women",
     title = "women's secrets",
-    user = "Ксения",
     listOfUsers = mutableListOf("Ксения", "Оксана", "Екатерина"),
+)
+val user = User(
+    userName = "Ксения",
     userStatus = "пользователь заглушен",
+    profilePicture = "рисунок",
 )
