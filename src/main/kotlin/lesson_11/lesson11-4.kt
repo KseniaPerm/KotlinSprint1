2afb32b4f49f7b1794a14c9aa9248982b00f1a8f
KPermyakova <k.e.permyakova@yandex.ru>
2025-03-31 16:39:06 +0500

    KS-11-4-creating-entities-for-recipe fixing

--- src/main/kotlin/lesson_11/lesson11-4.kt
+++ src/main/kotlin/lesson_11/lesson11-4.kt
@@ -3,19 +3,18 @@ package org.example.lesson_11
 class Category(
     val id: Int,
     val name: String,
-) {}
-
+    val recipes: List<String>,
+)
 class Recipe(
     val id: Int,
     val name: String,
-    val ingredients: String,
+    val ingredients: List<String>,
     val preparing: String,
     val portion: Int,
     val inFavourites: Boolean,
-) {}
-
+)
 class Ingredient(
     val id: Int,
     val name: String,
     val ingredientQuantity: Int,
-) {}
+)
