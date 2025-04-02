e25a0ded6f531397f11e6b4f8471a4a085766463
KPermyakova <k.e.permyakova@yandex.ru>
2025-04-02 16:12:35 +0500

    KS 11-5 create class Forum with builder

--- src/main/kotlin/lesson_11/lesson11-5.kt
+++ src/main/kotlin/lesson_11/lesson11-5.kt
@@ -1,36 +1,74 @@
 package org.example.lesson_11
 
-class Forum(
-    var userId: Int?,
-    var userName: String?,
-    var authorId: Int?,
-    var message: String?,
-) {
-    class ForumBuilder(
-        var userId: Int? = 0,
-        var userName: String? = " ",
-        var authorId: Int? = 0,
-        var message: String? = " ",
-    ) {
-        fun setUserId(id: Int) = apply { userId = id }
-        fun setUserName(name: String) = apply { userName = name }
-        fun setAuthorId(author: Int) = apply { authorId = author }
-        fun setMessage(mess: String) = apply { message = mess }
+class MemberOfForum(
+    var userId: Int = 0,
+    var userName: String? = " ",
+)
 
-        fun build() = Forum(userId, userName, authorId, message)
+class MemberOfForumBuilder {
+    var userId: Int = 0
+    var userName: String = " "
+
+    fun setUserId(userId: Int) = apply { this.userId = userId }
+    fun setUserName(userName: String) = apply { this.userName = userName }
+    fun build() = MemberOfForum()
+}
+
+class ForumMessages(
+    val authorId: Int = 0,
+    val message: String = " ",
+)
+
+class ForumMessagesBuilder {
+    var authorId: Int = 0
+    var message: String = " "
+
+    fun setAuthorId(authorId: Int) = apply { this.authorId = authorId }
+    fun setMessage(message: String) = apply { this.message = message }
+    fun build() = ForumMessages()
+}
+
+class Forum {
+    var userId: Int = 0
+    var listOfMessages: MutableList<ForumMessages> = mutableListOf()
+    val listOfUsers: MutableList<MemberOfForum> = mutableListOf()
+
+    fun createNewUser(name: String): MemberOfForum {
+        val newUser = MemberOfForumBuilder()
+            .setUserName(name)
+            .setUserId(userId)
+            .build()
+        listOfUsers.add(newUser)
+        return newUser
     }
+
+    fun createNewMessage(authorId: Int, message: String) {
+        if (this.userId == authorId) {
+            val newMessage = ForumMessagesBuilder()
+                .setAuthorId(authorId)
+                .setMessage(message)
+                .build()
+            listOfMessages.add(newMessage)
+        } else {
+            println("Такого пользователя нет ")
+        }
+    }
+    //fun printThread(message: String) {
+    //}
 }
 
 fun main() {
-
+    val forum = Forum()
+    println("Введите имя пользователя: ")
+    val newUserName = readln()
+    val user1 = forum.createNewUser(newUserName)
+    println("Введите сообщение : ")
+    val message1 = readln()
+    forum.createNewMessage(user1.userId, message1)
+    println("Введите имя пользователя: ")
+    val newUserName1 = readln()
+    val user2 = forum.createNewUser(newUserName1)
+    println("Введите сообщение: ")
+    val message2 = readln()
+    forum.createNewMessage(user2.userId, message2)
 }
-
-class MemberOfForumBuilder(
-    val userId: Int,
-    val userName: String,
-) {}
-
-class ForumMessages(
-    val authorId: Int,
-    val message: String,
-) {}
