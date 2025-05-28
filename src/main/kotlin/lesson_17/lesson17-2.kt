28a95e6a9c88bc4f2899c46583fda1425c7c39be
KPermyakova <k.e.permyakova@yandex.ru>
2025-05-28 14:39:57 +0500

    KS17-2-create-class-Ship fixing

--- src/main/kotlin/lesson_17/lesson17-2.kt
+++ src/main/kotlin/lesson_17/lesson17-2.kt
@@ -1,19 +1,18 @@
 package org.example.lesson_17
 
-class Ship(private val name: String, val averageSpeed: Int, val homePort: String) {
-    fun getName(): String {
-        return name
-    }
-
-    fun changeName(newName: String) {
-        println("Название корабля менять нельзя!")
-    }
+class Ship {
+    var name: String
+        get() = "Лолита"
+        set(value) {
+            println("Название корабля нельзя менять!")
+        }
+    val averageSpeed = 60
+    val homePort = "Сочи"
 }
 
 fun main() {
-    val ship = Ship("Лолита", 60, "Сочи")
-    println("Название корабля: ${ship.getName()}")
-    println("Средняя скорость корабля: ${ship.averageSpeed}")
-    println("Порт приписки корабля: ${ship.homePort}")
-    ship.changeName("Летний")
+    val ship = Ship()
+    println(ship.name)
+    ship.name = "Ленин"
+    println(ship.name)
 }
