fe3385124fe2b0fb5665cfdbc93da5bae5d7632f
KPermyakova <k.e.permyakova@yandex.ru>
2025-05-29 16:04:17 +0500

    KS17-3-create-class-folder

--- src/main/kotlin/lesson_17/lesson17-3.kt
+++ src/main/kotlin/lesson_17/lesson17-3.kt
@@ -1,21 +1,20 @@
 package org.example.lesson_17
 
 class Folder(
-    private var isSecret: Boolean,
+    private var isSecret: Boolean = true,
 ) {
-    val name: String
-        get() = if (isSecret) {
-            "Скрытая папака"
-        } else {
-            "Школа 1"
+    val name2 = "Скрытая строка"
+
+    var name = "Школа 1"
+        get() = if (isSecret) field else name2
+        set(value) {
+            field = value
+            println("Папка: $name}")
         }
-    val numberOfFiles: Int
-        get() = if (isSecret) {
-            0
-        } else {
-            12
-        }
+    val numberOfFiles: Int = 12
+        get() = if (isSecret) field else 0
 }
+
 
 fun main() {
     val folder = Folder(false)
