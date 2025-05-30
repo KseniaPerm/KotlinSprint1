77e1b94b7c968e646cfbc0ce3a334b7ca2eb5541
KPermyakova <k.e.permyakova@yandex.ru>
2025-05-30 14:16:02 +0500

    KS17-3-create-class-folder fixing with setter

--- src/main/kotlin/lesson_17/lesson17-3.kt
+++ src/main/kotlin/lesson_17/lesson17-3.kt
@@ -3,20 +3,16 @@ package org.example.lesson_17
 class Folder(
     private var isSecret: Boolean = true,
 ) {
-    val name2 = "Скрытая строка"
+    private val name2 = "Скрытая строка"
 
     var name = "Школа 1"
         get() = if (isSecret) field else name2
-        set(value) {
-            field = value
-            println("Папка: $name}")
-        }
     val numberOfFiles: Int = 12
         get() = if (isSecret) field else 0
 }
 
 fun main() {
-    val folder = Folder(false)
+    val folder = Folder(true)
     println("Папка: ${folder.name}")
     println("Количество файлов: ${folder.numberOfFiles}")
 }
