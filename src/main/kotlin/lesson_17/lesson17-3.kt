3dfd0a757053e23cf710e88951ed1c408a4e45a4
KPermyakova <k.e.permyakova@yandex.ru>
2025-06-02 11:03:19 +0500

    KS17-3-create-class-folder fixing with getter

--- src/main/kotlin/lesson_17/lesson17-3.kt
+++ src/main/kotlin/lesson_17/lesson17-3.kt
@@ -1,18 +1,18 @@
 package org.example.lesson_17
 
 class Folder(
+    private val name: String,
+    private val numberOfFiles: Int,
     private var isSecret: Boolean = true,
 ) {
-    private val name2 = "Скрытая строка"
-
-    var name = "Школа 1"
-        get() = if (isSecret) field else name2
-    val numberOfFiles: Int = 12
-        get() = if (isSecret) field else 0
+    val name1 : String = name
+        get() = if (!isSecret) field else "Скрытая папка"
+    val numberOfFiles1: Int = 12
+        get() = if (!isSecret) field else 0
 }
 
 fun main() {
-    val folder = Folder(true)
-    println("Папка: ${folder.name}")
-    println("Количество файлов: ${folder.numberOfFiles}")
+    val folder = Folder("Школа 1",12, true)
+    println("Папка: ${folder.name1}")
+    println("Количество файлов: ${folder.numberOfFiles1}")
 }
