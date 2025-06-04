87ec3a32f10906b7865b0cfd5b1e7deef39bdbb3
KPermyakova <k.e.permyakova@yandex.ru>
2025-06-04 12:07:39 +0500

    KS-18-1-create-class-orders fixing

--- src/main/kotlin/lesson_18/lesson18-1.kt
+++ src/main/kotlin/lesson_18/lesson18-1.kt
@@ -1,26 +1,20 @@
 package org.example.lesson_18
 
 class Orders(val orderNumber: Int) {
-    fun printInfo(
-
-        nameOfItem: String,
-    ) {
+    fun printInfo(nameOfItem: String) {
         println("Заказан товар: $nameOfItem")
     }
 
-    fun printInfo(
-        nameOfItem: List<String>,
-    ) {
+    fun printInfo(nameOfItem: List<String>) {
         println("Заказаны товары: ${nameOfItem.joinToString()}")
     }
 }
 
 fun main() {
     val order = Orders(1)
-    order.printInfo(
-        "Рюкзак походный"
-    )
+    order.printInfo("Рюкзак походный")
     order.printInfo(listOf("Рюкзак походный", "Газовый баллон", "Палатка"))
+
     val order2 = Orders(2)
     order2.printInfo("Котелок")
     order2.printInfo(listOf("Котелок", "Кастрюля", "Чайник"))
