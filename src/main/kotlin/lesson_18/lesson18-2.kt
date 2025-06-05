ad69172f27c1c1b03d24247c207434bae30ba831
KPermyakova <k.e.permyakova@yandex.ru>
2025-06-05 15:34:43 +0500

    KS-18-2-create-classes-for-dice fixing

--- src/main/kotlin/lesson_18/lesson18-2.kt
+++ src/main/kotlin/lesson_18/lesson18-2.kt
@@ -1,44 +1,32 @@
 package org.example.lesson_18
 
-open class Dice {
-    open fun getRollOfDice() = 0
+abstract class Dice {
+    abstract val numberOfDice: Int
 
-    class FourDiceFaces(private val numberOfDice: Int = 4) : Dice() {
-        override fun getRollOfDice(): Int {
-            val number = (1..numberOfDice).random()
-            println("На кубике выпало: $number")
-            return number
-        }
-    }
-
-    class SixDiceFaces(private val numberOfDice: Int = 6) : Dice() {
-        override fun getRollOfDice(): Int {
-            val number = (1..numberOfDice).random()
-            println("На кубике выпало: $number")
-            return number
-        }
-    }
-
-    class EightDiceFaces(private val numberOfDice: Int = 8) : Dice() {
-        override fun getRollOfDice(): Int {
-            val number = (1..numberOfDice).random()
-            println("На кубике выпало: $number")
-            return number
-        }
+    open fun getRollOfDice(): Int {
+        val number = (1..numberOfDice).random()
+        println("На кубике выпало: $number")
+        return number
     }
 }
 
+class FourDiceFaces(override val numberOfDice: Int) : Dice()
+
+class SixDiceFaces(override val numberOfDice: Int) : Dice()
+
+
+class EightDiceFaces(override val numberOfDice: Int) : Dice()
+
+
 fun main() {
-    val fourDiceFaces = Dice.FourDiceFaces()
+    val fourDiceFaces = FourDiceFaces(4)
     fourDiceFaces.getRollOfDice()
-    val sixDiceFaces = Dice.SixDiceFaces()
+    val sixDiceFaces = SixDiceFaces(6)
     sixDiceFaces.getRollOfDice()
-    val eightDiceFaces = Dice.EightDiceFaces()
+    val eightDiceFaces = EightDiceFaces(8)
     eightDiceFaces.getRollOfDice()
     println()
 
-    val dice: List<Dice> = listOf(fourDiceFaces, sixDiceFaces, eightDiceFaces)
-    for (i in dice) {
-        i.getRollOfDice()
+   val dice: List<Dice> = listOf(fourDiceFaces, sixDiceFaces, eightDiceFaces)
+    dice.forEach { i -> i.getRollOfDice() }
     }
-}
