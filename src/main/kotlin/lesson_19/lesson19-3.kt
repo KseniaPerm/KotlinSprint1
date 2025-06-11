67ca3ffcfa9cca7c37cc53fc730f759a867232cb
KPermyakova <k.e.permyakova@yandex.ru>
2025-06-11 12:46:07 +0500

    KS-19-3-create-class-ShipActions fixing

--- src/main/kotlin/lesson_19/lesson19-3.kt
+++ src/main/kotlin/lesson_19/lesson19-3.kt
@@ -1,25 +1,25 @@
 package org.example.lesson_19
 
+class SpaceShip {
+
+    fun takeOff() {
+        println("Корабль взлетает.")
+        //TODO добавить реализацию метода
+    }
+
+    fun shooting() {
+        println("Корабль отстреливается от астероида.")
+    }
+
+    fun landing() {
+        println("Корабль приземляется.")
+        TODO("Добавить дополнительную логику")
+    }
+}
+
 fun main() {
-    val actions = listOf(ShipActions.TAKE_OFF, ShipActions.LANDING, ShipActions.SHOOT_BACK, ShipActions.ERROR)
-    for (i in actions) {
-        setAction(i)
-    }
-}
-
-enum class ShipActions(val actionName: String) {
-    TAKE_OFF("взлетать"),
-    LANDING("приземляться"),
-    SHOOT_BACK("отстреливаться от астероида"),
-    ERROR("Ошибка"),
-}
-
-fun setAction(action: ShipActions) {
-    when (action) {
-        ShipActions.TAKE_OFF -> println("взлетать")
-           // TODO("нужна дополнительная логика()")
-        ShipActions.LANDING -> println("приземляться")
-        ShipActions.SHOOT_BACK -> println("отстреливаться от астероида")
-        ShipActions.ERROR -> {throw NotImplementedError()}
-    }
+    val spaceShip = SpaceShip()
+    spaceShip.takeOff()
+    spaceShip.shooting()
+    spaceShip.landing()
 }
