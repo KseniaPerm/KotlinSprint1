5aeeac3565501e18d11de0ac6e9bc0b55181c0b5
KPermyakova <k.e.permyakova@yandex.ru>
2025-06-18 12:53:29 +0500

    KS-20-4-create-list-of-lambda fixing

--- src/main/kotlin/lesson_20/lesson20-4.kt
+++ src/main/kotlin/lesson_20/lesson20-4.kt
@@ -1,14 +1,13 @@
 package org.example.lesson_20
 
 fun main() {
-    val listOfEFruits = listOf("Груша", "Яблоко", "Банан", "Апельсин", "Лимон")
-    val lambdaList: List<() -> String> = listOfEFruits.map { str -> { str } }
-    lambdaList.forEach { lambda ->
-        println("Нажат элемент : ${lambda()}")
+    val listOfFruits = listOf("Груша", "Яблоко", "Банан", "Апельсин", "Лимон")
+    val lambdaList: List<() -> Unit> = listOfFruits.map { fruit ->
+        { println("Нажат элемент $fruit") }
     }
 
-    lambdaList.forEachIndexed { index, lambda ->
-        if (index % 2 == 0)
-            println("Нажат элемент  ${lambda()}")
+    lambdaList.forEachIndexed { index, fruit ->
+        if (index % 2 == 1)
+            fruit()
     }
 }
