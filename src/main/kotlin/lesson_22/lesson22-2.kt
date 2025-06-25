b7ab94848746965a125925659831f6beb966a1f2
KPermyakova <k.e.permyakova@yandex.ru>
2025-06-25 12:12:07 +0500

    KS-22-2-output-the-object-to-console fixing

--- src/main/kotlin/lesson_22/lesson22-2.kt
+++ src/main/kotlin/lesson_22/lesson22-2.kt
@@ -1,19 +1,18 @@
 package org.example.lesson_22
 
-
-class SadBook(val name: String, private val author: String){
+class SadBook(val name: String, private val author: String) {
     override fun toString(): String {
-        return "Word (name = '$name', author = '$author')"
+        return "(name = '$name', author = '$author')"
     }
 }
 
 data class FunBook(val name: String, val author: String)
 
-fun main(){
+fun main() {
     val sadBook = SadBook("Му-му", "Иван Тургенев")
     val funBook = FunBook("Робинзон Крузо", "Даниэль Дефо")
     println(sadBook)
-    // при выводе объекта обычного класса, переопределили функцию toString(), поэтому вывод нас устраивает. Иначе была бы непонятная строка в консоли.
+    // при выводе объекта обычного класса, переопределили функцию toString(), поэтому вывод нас устраивает. Иначе выводился бы стандартный вид — имя класса и хэш.
     println(funBook)
     // при выводе в консоль объекта дата класса, доп.манипуляций не нужно, просто печатаем через println() название объекта
 }
